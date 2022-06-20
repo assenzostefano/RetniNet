1abccd1661cda3b14a4fd3364070123b5b6f5437
stef-devs <stef58a@gmail.com>
2022-06-20 09:35:44 +0200

    Add translate for command meteo and comments on the code

--- index.py
+++ index.py
@@ -16,6 +16,8 @@ import json
 import goslate
 import urllib.request
 import urllib.parse
+import logging
+import deepl
 from dotenv import load_dotenv
 from yt_dlp import YoutubeDL
 from platform import system
@@ -83,14 +85,19 @@ def meteo(pm):
     bot.register_next_step_handler(sent_msg, meteo_step)
 
 def meteo_step(message):
+    translator = deepl.Translator(os.getenv('DEEPL_TOKEN'))
+    result = translator.translate_text('hi', target_lang='IT') 
+    translated_text = result.text
     city = message.text
     token_weather = os.environ.get('WEATHER_TOKEN')
     response = requests.get("https://api.openweathermap.org/data/2.5/weather?q="+city+",it&APPID="+token_weather).json()
-    weather = response['weather'][0]['main']
+    weather = response['weather'][0]['main'] #
     temp = response['main']['temp']
     temp = temp - 273.15
+    weather_translate = translator.translate_text(weather, target_lang='IT')
+    result_weather = weather_translate.text
     bot.send_message(message.chat.id, "🌡️ La temperatura in " + city + " è di " + str(temp) + "°C")
-    bot.send_message(message.chat.id, "🌧️ La condizione è " + weather)
+    bot.send_message(message.chat.id, "🌧️ La condizione è " + result_weather)
 
 #Command /stats
 @bot.message_handler(commands=['stats'])
@@ -143,27 +150,50 @@ def epicgames(message):
 def epicgames_step(message):
     text = message.text
     if text == 'disponibile':
-        print("Triggered command EPICGAMES.")
+        print("Triggered command EPICGAMES DISPONIBILE.")
+        #URL API
         url = "https://api.plenusbot.xyz/epic_games?country=IT"
         response = requests.get(url).json()
+        #Title of current games
         current_games = response['currentGames'][0]['title']
+        #Image current games
         image_currentgames = response['currentGames'][0]['keyImages'][0]['url']
+        #Description current games
         current_games_description = response['currentGames'][0]['description']
+        #Token for translate
+        translator = deepl.Translator(os.getenv('DEEPL_TOKEN'))
+        #Translate description current games
+        description_translate = translator.translate_text(current_games_description, target_lang='IT')
+        result_description = description_translate.text
         send_img = bot.send_photo(message.chat.id, image_currentgames)
-        sent_msg = bot.send_message(message.chat.id, "🎮 Il gioco gratis di oggi è " + current_games + "\n\n" + current_games_description)
+        sent_msg = bot.send_message(message.chat.id, "🎮 Il gioco gratis di oggi è " + current_games + "\n\n" + result_description)
     else:
+        print("Triggered command EPICGAMES FUTURO.")
+        #URL API
         url = "https://api.plenusbot.xyz/epic_games?country=IT"
         response = requests.get(url).json()
+        #Title of future games
         future_games1 = response['nextGames'][0]['title']
+        #Image future games
         image_futuregames1 = response['nextGames'][0]['keyImages'][0]['url']
+        #Description future games
         future_games_description1 = response['nextGames'][0]['description']
+        #Token for translate
+        translator = deepl.Translator(os.getenv('DEEPL_TOKEN'))
+        description_translate1 = translator.translate_text(future_games_description1, target_lang='IT')
+        result_description1 = description_translate1.text
         send_img = bot.send_photo(message.chat.id, image_futuregames1)
-        sent_msg = bot.send_message(message.chat.id, "🎮 Il gioco futuro è " + future_games1 + "\n\n" + future_games_description1)
+        sent_msg = bot.send_message(message.chat.id, "🎮 Il gioco futuro è " + future_games1 + "\n\n" + result_description1)
+        #Title of future games
         future_games2 = response['nextGames'][1]['title']
+        #Image future games
         image_futuregames2 = response['nextGames'][1]['keyImages'][0]['url']
+        #Description future games
         future_games_description2 = response['nextGames'][1]['description']
+        #Traslate description future games
+        description_translate2 = translator.translate_text(future_games_description2, target_lang='IT')
+        result_description2 = description_translate2.text
         send_img = bot.send_photo(message.chat.id, image_futuregames2)
-        sent_msg = bot.send_message(message.chat.id, "🎮 Il gioco futuro è " + future_games2 + "\n\n" + future_games_description2)
-
+        sent_msg = bot.send_message(message.chat.id, "🎮 Il gioco futuro è " + future_games2 + "\n\n" + result_description2)
 
 bot.polling()
